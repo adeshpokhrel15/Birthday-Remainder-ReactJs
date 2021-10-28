49a10c451f594dc135ae505bded1343b5a2fbf1f
Adesh Pokhrel <47937020+adeshpokhrel15@users.noreply.github.com>
2021-10-28 22:16:49 +0545

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    name: 'Sachin Sapkota',
+    age: 20,
+    image:'https://res.cloudinary.com/adeshpokhrel/image/upload/v1635438229/React/sachin_sdo91z.png',
+  },
+  {
+    id: 2,
+    name: 'Ashwin Khaitwada',
+    age: 32,
+    image:'https://res.cloudinary.com/adeshpokhrel/image/upload/v1635438229/React/ash_t3km6f.png',
+    },
+  {
+    id: 3,
+    name: 'Prasmit Neupane',
+    age: 36,
+    image: 'https://res.cloudinary.com/adeshpokhrel/image/upload/v1635438229/React/prasmit_jfjjox.png',
+    
+  },
+  {
+    id: 4,
+    name: 'Mahesh Chapagain',
+    age: 34,
+    image:'https://res.cloudinary.com/adeshpokhrel/image/upload/v1635438229/React/mahesh_clfcnj.png',
+  },
+];
